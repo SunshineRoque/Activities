f15645ca1d1f9a2a4fe63848ab6fb589984401cc
samsondanicah <sunshineararoque@gmail.com>
2023-09-14 15:08:07 +0800

    Array Question 01 - Uniq Method

---
+++ array_activities/question_1.rb
@@ -0,0 +1,11 @@
+# UNIQ METHOD
+
+#Make a array by taking 10 input from user. Now delete all repeated elements of the array.
+#e.g.
+#INPUT : [1,2,3,2,1,3,12,12,32]
+#OUTPUT : [1,2,3,12,32]
+
+a=[1,2,3,2,1,3,12,12,32]
+p "Remove duplicate numbers in #{a}"
+
+p "Answer #{a.uniq}"
