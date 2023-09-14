ed5d6e12025fc3d446240622485e7660fb7a4300
samsondanicah <sunshineararoque@gmail.com>
2023-09-14 15:14:13 +0800

    Array Question 09 - Concatenate Method

---
+++ array_activities/question_9.rb
@@ -0,0 +1,14 @@
+#Concatenate Method
+
+#Write a Ruby program to concatenate array of arrays into one.
+
+array1 = [1, 2, 3]
+array2 = [4, 5, 6]
+a1="I Love "
+a2="Jesus"
+
+array1.concat(array2)
+c=a1+a2
+
+puts array1.inspect
+puts c
