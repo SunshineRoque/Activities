0608ba858777eb2cfe71eb8c9f677c4e46e4546b
samsondanicah <sunshineararoque@gmail.com>
2023-09-18 11:33:26 +0800

    Class Method Exercise 01 - Odd or Even Number using Class Method

---
+++ class_method_activities/class_method_exercise_1.rb
@@ -0,0 +1,20 @@
+#Determine Odd and Even Numbers using Class Method
+
+class OddEven
+  def self.odd_even(num)
+    if num % 2 == 0
+      "Even Number"
+
+    else
+      "Odd Number"
+    end
+  end
+end
+
+print "Enter a number:"
+number = gets.chomp.to_i
+
+
+result = OddEven.odd_even(number)
+
+puts "#{number} is #{result}"
