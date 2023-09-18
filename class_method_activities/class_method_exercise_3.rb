dc653e59dc326a55529d488a52f18569bcff4992
samsondanicah <sunshineararoque@gmail.com>
2023-09-18 11:35:19 +0800

    Class Method Exercise 03 - Letter Grade Using Class Method

---
+++ class_method_activities/class_method_exercise_3.rb
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+class LetterGrade
+
+  def initialize(score)
+    @score = score
+  end
+
+  def num
+    if @score>=0 && @score<=59
+      g='F'
+    elsif @score>=60 && @score<=69
+      g='D'
+    elsif @score>=70 && @score<=79
+      g='C'
+    elsif @score>=80 && @score<=89
+      g='B'
+    elsif @score>=90 && @score<=100
+      g='A'
+    else
+      g='Invalid Score'
+    end
+    g
+  end
+end
+
+print "Enter your grade: "
+grade=gets.chomp.to_i
+
+puts "\n"
+
+result = LetterGrade.new(grade)
+a=result.num
+print "Your Letter Grade is #{a}"
+
+
