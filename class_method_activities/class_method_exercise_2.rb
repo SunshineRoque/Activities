68d0bb4b86c9a0e227a932190bfdec2d3b3c79e1
samsondanicah <sunshineararoque@gmail.com>
2023-09-18 11:34:34 +0800

    Class Method Exercise 02 - Introduce Yourself

---
+++ class_method_activities/class_method_exercise_2.rb
@@ -0,0 +1,22 @@
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def who
+    puts "Hello, I am #{@name} and I am #{@age} years old."
+  end
+end
+
+
+print "Enter your name: "
+input_info_name = gets.chomp
+
+print "Enter your age: "
+input_info_age = gets.chomp.to_i
+puts "\n"
+
+person = Person.new(input_info_name, input_info_age)
+person.who
